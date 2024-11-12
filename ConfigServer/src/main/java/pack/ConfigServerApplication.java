5c75b659340c4b8b370b19e60f54d86479445565
SeungminL2 <mslsm111@gmail.com>
2024-11-12 13:38:19 +0900

    테스트 - 승민

--- ConfigServer/src/main/java/pack/ConfigServerApplication.java
+++ ConfigServer/src/main/java/pack/ConfigServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// 테슽트테스트 - 승민
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
