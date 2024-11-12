20d4129ec044822283a1ceb8d15370d08307af26
yejinChoi <122140187+yejinchoi24@users.noreply.github.com>
2024-11-12 14:03:18 +0900

    직접 실습해보는 테스트용 밋 ㅎㅎ^^ (#5)
    * 직접 실습해보는 테스트용 밋 ㅎㅎ^^
    
    * Add .properties, target, and factory files to .gitignore

--- HospitalService/src/main/java/pack/HospitalServiceApplication.java
+++ HospitalService/src/main/java/pack/HospitalServiceApplication.java
@@ -2,7 +2,7 @@ package pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// 테스트
+// 테스트//내가 직접 실습해보는테스트용 커밋 -최예
 @SpringBootApplication
 public class HospitalServiceApplication {
 
