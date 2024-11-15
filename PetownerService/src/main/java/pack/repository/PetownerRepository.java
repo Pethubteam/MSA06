9c6e46fd60f8d0c0927442ddb893f57713d29843
SeungminL2 <mslsm111@gmail.com>
2024-11-15 14:17:09 +0900

    이클립스 파일

---
+++ PetownerService/src/main/java/pack/repository/PetownerRepository.java
@@ -0,0 +1,13 @@
+package pack.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pack.domain.Petowner;
+
+@Repository
+public interface PetownerRepository extends JpaRepository<Petowner, Long> {
+	Optional<Petowner> findByEmail(String email);
+}
